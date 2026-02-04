53c267459bf317b277cdbf4a7c64c05ec27ce995
Arthur Formigoni Santos Da Cruz <arthurcruz-ieg@instituto.ad>
2026-02-04 08:58:17 -0300

    initial commit

---
+++ src/main/java/com/example/plataformadioggoapi/PlataformaDioggoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.plataformadioggoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDioggoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaDioggoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/plataformadioggoapi/PlataformaDioggoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plataformadioggoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDioggoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
